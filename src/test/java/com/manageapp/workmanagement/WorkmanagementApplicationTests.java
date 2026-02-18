01a1ec03655f2ecebea210376928851073338db8
Manoj <khanavalkarmanoj@gmail.com>
2026-02-18 22:36:03 +0530

    first commit

---
+++ src/test/java/com/manageapp/workmanagement/WorkmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manageapp.workmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
